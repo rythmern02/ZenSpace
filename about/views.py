db2422e87cacac96fc84f7d2dd7f447cab92b2ca
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2023-03-26 13:25:03 +0530

    added files

---
+++ about/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'about/about.html')
+
+def learnm(request):
+    return render(request,'about/learnm.html')
